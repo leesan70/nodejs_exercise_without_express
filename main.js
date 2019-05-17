35fed8bf454fbf5367bd37a870d797c0c4f7ddf7
Sang Bin (Kevin) Lee <kevinlt@hotmail.com>
2019-05-17 16:29:29 +0900

    Change links to be populated by the static files existing in  directory

--- main.js
+++ main.js
@@ -9,29 +9,30 @@ var app = http.createServer(function(request,response){
     var title = queryData.id === undefined ? 'Welcome' : queryData.id;
  
     if(pathname == '/'){
-      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${title === 'Welcome' ? 'Hello, Node.js' : description}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-      });
+      fs.readdir('./data', function(err, filelist) {
+        var hrefs = '<ul>';
+        hrefs += filelist.map((filename, index) => `<li><a href="/?id=${filename}">${filename}</a></li>`).join('');
+        hrefs += '</ul>';
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${hrefs}
+            <h2>${title}</h2>
+            <p>${title === 'Welcome' ? 'Hello, Node.js' : description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      })
     } else {
       response.writeHead(404);
       response.end('Not found');
