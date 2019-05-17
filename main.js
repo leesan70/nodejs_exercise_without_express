56537e022ce9ee6e3816766f1d5e43b020b4e968
Sang Bin (Kevin) Lee <kevinlt@hotmail.com>
2019-05-17 16:51:06 +0900

    Seaprate out templates into functions

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
- 
+
+function HTMLtemplate(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function links(filelist) {
+  var hrefs = '<ul>';
+  hrefs += filelist.map(
+    (filename, index) => `<li><a href="/?id=${filename}">${filename}</a></li>`
+  ).join('');
+  hrefs += '</ul>';
+  return hrefs;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -10,25 +36,12 @@ var app = http.createServer(function(request,response){
  
     if(pathname == '/'){
       fs.readdir('./data', function(err, filelist) {
-        var hrefs = '<ul>';
-        hrefs += filelist.map((filename, index) => `<li><a href="/?id=${filename}">${filename}</a></li>`).join('');
-        hrefs += '</ul>';
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${hrefs}
-            <h2>${title}</h2>
-            <p>${title === 'Welcome' ? 'Hello, Node.js' : description}</p>
-          </body>
-          </html>
-          `;
+          var template = HTMLtemplate(
+            title,
+            links(filelist),
+            `<p>${title === 'Welcome' ? 'Hello, Node.js' : description}</p>`
+          );
           response.writeHead(200);
           response.end(template);
         });
