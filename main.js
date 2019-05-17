9fba41594b5a05bcbe4a25fef67de85776413952
Sang Bin (Kevin) Lee <kevinlt@hotmail.com>
2019-05-17 17:53:29 +0900

    Add HTML form to support  feature.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function HTMLtemplate(title, list, body){
   <!doctype html>
   <html>
   <head>
-    <title>WEB1 - ${title}</title>
+    <title>${title}</title>
     <meta charset="utf-8">
   </head>
   <body>
@@ -24,6 +24,7 @@ function links(filelist) {
   hrefs += filelist.map(
     (filename, index) => `<li><a href="/?id=${filename}">${filename}</a></li>`
   ).join('');
+  hrefs += '<a href="/create">create</a>';
   hrefs += '</ul>';
   return hrefs;
 }
@@ -32,20 +33,39 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id === undefined ? 'Welcome' : queryData.id;
  
-    if(pathname == '/'){
+    if(pathname === '/'){
       fs.readdir('./data', function(err, filelist) {
+        var title = queryData.id === undefined ? 'Welcome' : queryData.id;
+        var list = links(filelist);
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
           var template = HTMLtemplate(
             title,
-            links(filelist),
+            list,
             `<p>${title === 'Welcome' ? 'Hello, Node.js' : description}</p>`
           );
           response.writeHead(200);
           response.end(template);
         });
       })
+    } else if(pathname === '/create'){
+      fs.readdir('./data', function(err, filelist){
+        var title = 'WEB - create';
+        var list = links(filelist);
+        var template = HTMLtemplate(title, list, `
+          <form action="http://localhost:3000/process_create" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `);
+        response.writeHead(200);
+        response.end(template);
+      });
     } else {
       response.writeHead(404);
       response.end('Not found');
